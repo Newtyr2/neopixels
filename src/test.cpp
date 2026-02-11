93c0b4ea8e7bf0162d3c3462b88e63fd62f8d53f
Newtyr2 <hevkox@gmail.com>
2026-02-10 20:42:00 -0600

    New mapping

--- src/test.cpp
+++ src/test.cpp
@@ -5,15 +5,26 @@ using namespace std;
 //#include <iostream>
 
 
-vector<int> mapping = {8, 9, 10, 11, 12, 13, 14, 15 , 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 2, 1, 0, 8, 9, 10, 11, 12, 13, 14, 15};
+vector<int> mapping = {8, 9, 10, 11, 12, 13, 14, 15, 7, 6, 5, 4, 3, 2, 1, 0, 0, 1, 2, 3, 4, 5, 6, -1, 7, 6, 5, 4, 3, 2, 1, 0, 8, 9, 10, 11, 12, 13, 14, 15};
+
+struct Color {
+    float r, g, b, a;
+};
+
+// struct Lights{
+//     Color a;
+// };
 
 struct Bar{
-    vector<int> l;
+    vector<Color> l;
 };
 
 class Neopixels {
     array<Bar, 5> bars;
 
+    int rightIndex(int bar, int index){
+        return mapping[bar*8 + index];
+    }
 };
 
 
